8bda8cf5bd3b9fa72d6b62a3c84f28df508d8030
Winnie Phan <nhuphan0404@gmail.com>
2018-05-02 11:52:30 +0700

    final

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 
-$(document).ready(function(){
-  $(window).on("scroll", function() {
-      if($(window).scrollTop() > 10) {
-          $(".header").addClass("active");
-      } else {
-          //remove the background property so it comes transparent again (defined in your css)
-         $(".header").removeClass("active");
-      }
-    });
-  });
+// $(document).ready(function(){
+//   $(window).on("scroll", function() {
+//     if($(window).scrollTop() > 10) {
+//         $(".header").addClass("active");
+//     } else {
+//         $(".header").removeClass("active");
+//     }
+//   });
+// });
   
   
   
